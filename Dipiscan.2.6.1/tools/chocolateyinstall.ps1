96e4150e4f38fd054bcebf61bd26c116e96eb616
bibimagic <b23.martial@gmail.com>
2022-02-17 13:30:35 +0100

    Add files via upload

--- Dipiscan.2.6.1/tools/chocolateyinstall.ps1
+++ Dipiscan.2.6.1/tools/chocolateyinstall.ps1
@@ -1,8 +1,7 @@
-﻿
 $packageArgs = @{
   packageName   	= 'Dipiscan'
   fileType      	= 'exe'
-  url           	= "https://www.dipisoft.com/download_plus.php?action=1&lng=fr&pg=3221"
+  url           	= "https://www.dipisoft.com/file/Dipiscan261.exe"
   softwareName  	= 'Dipiscan'
   checksum      	= '9B56AB3750D5A0EFD7FAA718B975ECA7CF88B101B42BB67B675FE8755F743B72'
   checksumType  	= 'sha256'
